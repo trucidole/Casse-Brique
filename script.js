292a9356f2924465bc747d5a1c5388d570ac866b
Glitch (projet-casse-brique) <none>
2020-04-07 09:12:58 +0000

    🦂🏃 Updated with Glitch

--- script.js
+++ script.js
@@ -4,14 +4,14 @@ var largjeux = 600;
 var longbrique = 50;
 var largbrique = 40;
 
-var coordx_barre = 400;
+var coordx_barre = 500;
 var longbarre = 100;
 var largbarre = 25;
 
 var deplacement_balle_sur_x = 300;
 var deplacement_balle_sur_y = 300;
 var taille_balle = 20;
-var vitesse_balle = 0.8;
+var vitesse_balle = 0.1;
 var direction_X = 1;
 var direction_Y = -1;
 
@@ -36,7 +36,7 @@ function setup() {
 }
 function draw() {
   //Barre
-  fill(255, 255, 255);
+  fill(255, 255, 100);
   rect(coordx_barre, largjeux - largbarre, longbarre, largbarre);
 
   //Balle
@@ -50,18 +50,38 @@ function draw() {
 
   // rebond balle
   // rebond balle gauche
-  if (deplacement_balle_sur_x <= 10) {
+  if (deplacement_balle_sur_x <= taille_balle / 2) {
     direction_X = 1;
   }
   // rebond balle droite
-  if (deplacement_balle_sur_x >= 890) {
+  if (deplacement_balle_sur_x >= longjeux - taille_balle / 2) {
     direction_X = -1;
   }
   // rebond balle haut
-
-  if (deplacement_balle_sur_y <= 10) {
+  if (deplacement_balle_sur_y <= taille_balle / 2) {
     direction_Y = 1;
   }
+  //rebond barre
+
+  if ((deplacement_balle_sur_x >= coordx_barre - taille_balle / 2) && (deplacement_balle_sur_x <= coordx_barre + longbarre + taille_balle / 2) && (deplacement_balle_sur_y >= largjeux - largbarre)) {
+      direction_Y = -1;
+    }
+  if(deplacement_balle_sur_y >= largjeux){
+    alert(Perdu)
+  }
+  
+    if (key == "q" || key == "Q") {
+      if (coordx_barre > 0) {
+        coordx_barre -= 1;
+       }
+      }
+      if (key == "d" || key == "D") {
+        if (coordx_barre < largjeux) {
+          coordx_barre += 1;
+        }
+      }
+
+  
   //context.clearrect(0, 0, longjeux, largjeux);
   // barre déplacement balle
   //bloc = new bloc(100, 40);
@@ -69,7 +89,7 @@ function draw() {
   //rafriachissement fenetre
   //fill(255, 0, 0);
 }
-setInterval(draw, 100);
+setInterval(draw,1000);
 
 function commandes() {
   if (keyPressed) {
@@ -77,6 +97,7 @@ function commandes() {
     if (key == "q" || key == "Q") {
       if (coordx_barre > 0) {
         coordx_barre -= 25;
+       }
       }
       if (key == "d" || key == "D") {
         if (coordx_barre < largjeux) {
@@ -84,5 +105,5 @@ function commandes() {
         }
       }
     }
-  }
+  
 }

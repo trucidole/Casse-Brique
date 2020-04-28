30b2ff0e00c519b71fd15feeb9eaf474c05ebbe9
Glitch (projet-casse-brique) <none>
2020-04-28 09:25:02 +0000

    🍨👯 Updated with Glitch

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ var largbarre = 25;
 var deplacement_balle_sur_x = 300;
 var deplacement_balle_sur_y = 300;
 var taille_balle = 20;
-var vitesse_balle = 0.1;
+var vitesse_balle = 1;
 var direction_X = 1;
 var direction_Y = -1;
 
@@ -25,16 +25,18 @@ function setup() {
   fill(255, 0, 255);
   stroke(0, 0, 0);
   background(0);
+
+  //brique;
+
   for (let u = 0; u < 200; u += 40) {
     for (let i = 0; i < width; i += 50) {
       fill(255, 0, 0);
       rect(i, u, longbrique, largbrique);
     }
-
-    //bloc;
   }
-}
-function draw() {
+  function draw() {
+    //Brique
+  }
   //Barre
   fill(255, 255, 100);
   rect(coordx_barre, largjeux - largbarre, longbarre, largbarre);
@@ -62,34 +64,37 @@ function draw() {
     direction_Y = 1;
   }
   //rebond barre
-
-  if ((deplacement_balle_sur_x >= coordx_barre - taille_balle / 2) && (deplacement_balle_sur_x <= coordx_barre + longbarre + taille_balle / 2) && (deplacement_balle_sur_y >= largjeux - largbarre)) {
-      direction_Y = -1;
-    }
-  if(deplacement_balle_sur_y >= largjeux){
-    alert(Perdu)
+  if (
+    deplacement_balle_sur_x >= coordx_barre - taille_balle / 2 &&
+    deplacement_balle_sur_x <= coordx_barre + longbarre + taille_balle / 2 &&
+    deplacement_balle_sur_y >= largjeux - largbarre
+  ) {
+    direction_Y = -1;
   }
-  
-    if (key == "q" || key == "Q") {
-      if (coordx_barre > 0) {
-        coordx_barre -= 1;
-       }
-      }
-      if (key == "d" || key == "D") {
-        if (coordx_barre < largjeux) {
-          coordx_barre += 1;
-        }
-      }
 
-  
-  //context.clearrect(0, 0, longjeux, largjeux);
+  //perdu
+  if (deplacement_balle_sur_y >= largjeux) {
+    alert("Perdu");
+  }
+  if (key == "q" || key == "Q") {
+    if (coordx_barre > 0) {
+      coordx_barre -= 1;
+    }
+  }
+  if (key == "d" || key == "D") {
+    if (coordx_barre + longbarre < longjeux) {
+      coordx_barre += 1;
+    }
+  }
+
+  //(0, 0, longjeux, largjeux);
   // barre déplacement balle
   //bloc = new bloc(100, 40);
 
   //rafriachissement fenetre
   //fill(255, 0, 0);
 }
-setInterval(draw,1000);
+setInterval(draw, 1000);
 
 function commandes() {
   if (keyPressed) {
@@ -97,13 +102,12 @@ function commandes() {
     if (key == "q" || key == "Q") {
       if (coordx_barre > 0) {
         coordx_barre -= 25;
-       }
-      }
-      if (key == "d" || key == "D") {
-        if (coordx_barre < largjeux) {
-          coordx_barre += 25;
-        }
       }
     }
-  
+    if (key == "d" || key == "D") {
+      if (coordx_barre < largjeux) {
+        coordx_barre += 25;
+      }
+    }
+  }
 }
